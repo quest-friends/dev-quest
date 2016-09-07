18db0278d58ac34198a80ed07305b511aa30b0ee
Dylan & Hudson Burns Prestidge <pair+dylanhul.hudson-prestidge@devbootcamp.com>
2016-09-07 19:43:25 +1200

    player movement reducers written, passing tests

--- reducers/player_moves.js
+++ reducers/player_moves.js
@@ -5,6 +5,18 @@ function playerMoves (state = {}, action) {
       newState = Object.assign({}, state)
       newState.player.position.x -= 1
       return newState
+    case('PLAYER_MOVE_RIGHT'):
+      newState = Object.assign({}, state)
+      newState.player.position.x += 1
+      return newState
+    case('PLAYER_MOVE_UP'):
+      newState = Object.assign({}, state)
+      newState.player.position.y -= 1
+      return newState
+    case('PLAYER_MOVE_DOWN'):
+      newState = Object.assign({}, state)
+      newState.player.position.y += 1
+      return newState
   }
 }
 module.exports = { playerMoves }
---
+++ tests/reducerTests.js
@@ -0,0 +1,34 @@
+const test = require('tape')
+const movement = require('../reducers/player_moves')
+
+test('PlayerMovesLeft', function (t) {
+  var testState = {player:{position: {x:10}  } }
+  var expectedState = {player:{position: {x:9}  } }
+  var actualState = movement.playerMoves(testState, {type: "PLAYER_MOVE_LEFT"})
+  t.deepEqual(actualState, expectedState, "Player moving left changes the state")
+  t.end()
+})
+
+test('PlayerMovesRight', function (t) {
+  var testState = {player:{position: {x:10}  } }
+  var expectedState = {player:{position: {x:11}  } }
+  var actualState = movement.playerMoves(testState, {type: "PLAYER_MOVE_RIGHT"})
+  t.deepEqual(actualState, expectedState, "Player moving right changes the state")
+  t.end()
+})
+
+test('PlayerMovesUp', function (t) {
+  var testState = {player:{position: {y:10}  } }
+  var expectedState = {player:{position: {y:9}  } }
+  var actualState = movement.playerMoves(testState, {type: "PLAYER_MOVE_UP"})
+  t.deepEqual(actualState, expectedState, "Player moving up changes the state")
+  t.end()
+})
+
+test('PlayerMovesDown', function (t) {
+  var testState = {player:{position: {y:10}  } }
+  var expectedState = {player:{position: {y:11}  } }
+  var actualState = movement.playerMoves(testState, {type: "PLAYER_MOVE_DOWN"})
+  t.deepEqual(actualState, expectedState, "Player moving down changes the state")
+  t.end()
+})
