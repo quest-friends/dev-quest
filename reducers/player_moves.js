eba991f83e72c101509bc7f7c606399a688b7252
Dylan & Hudson Burns Prestidge <pair+dylanhul.hudson-prestidge@devbootcamp.com>
2016-09-07 18:01:23 +1200

    setup reducers & tests file

---
+++ reducers/player_moves.js
@@ -0,0 +1,9 @@
+export default playerMoves (state = {}, action) => {
+  let newState = {}
+  switch(action.type){
+    case('PLAYER_MOVE_LEFT'):
+      newState = Object.assign({}, state)
+      newState.player.coords[0]-=1
+      return newState
+  }
+}
---
+++ tests/reducers.js
@@ -0,0 +1,4 @@
+var tape = require('tape')
+var playerMoves = require('../reducers/player_moves')
+
+console.log('playerMoves');
