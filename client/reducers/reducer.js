680c727231dc3cfa56149cd61ba6b3621f8b0452
Dylan <email": null>
2016-09-09 14:26:55 +1200

    playerwall test

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -3,13 +3,20 @@ import { combineReducers } from 'Redux'
 
 function reducer (state = initialState, action) {
   let newState = {}
+  let i = state.player.position.y
+  let j = state.player.position.x
+  const tileGrid = state.tileGrid
+
   switch(action.type){
 
     //these are the cases for player movement
     case('PLAYER_MOVE_LEFT'):
-      newState = Object.assign({}, state)
-      newState.player.position.x -= 1
-      return newState
+      if (tileGrid[i-1][j] != 0) {
+        console.log("this is tile to left: ", tileGrid[i-1][j]);
+        newState = Object.assign({}, state)
+        newState.player.position.x -= 1
+        return newState
+      }
     case('PLAYER_MOVE_RIGHT'):
       newState = Object.assign({}, state)
       newState.player.position.x += 1
