d187a1f4c75ebdb2dfedd8f721e80f8579448213
Dylan <email": null>
2016-09-09 20:36:15 +1200

    Multiple enemies now in enemies array and are rendered on the board - removed stray comments

--- client/connectors/boardConnector.js
+++ client/connectors/boardConnector.js
@@ -8,10 +8,6 @@ const mapStateToProps = (state) => {
     playerPosition: state.player.position,
     playerX: state.player.position.x,
     playerY: state.player.position.y,
-//note - we should check if these are necessary as enemies probably render w player
-    // enemyX: state.enemies.enemy.position.x,
-    // enemyY: state.enemies.enemy.position.y,
-//this is now an array from the state
     enemies: state.enemies
   }
 }
