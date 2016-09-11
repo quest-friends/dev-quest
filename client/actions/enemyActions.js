628710678b49d460d0b6d22fee69f3cc628783f2
Hudson Prestidge <charcon@ihug.co.nz>
2016-09-11 21:20:26 +1200

    attempt to prevent enemies moving into other enemies

--- client/actions/enemyActions.js
+++ client/actions/enemyActions.js
@@ -4,7 +4,35 @@ const allEnemiesAct = () => {
     type: "ALL_ENEMIES_ACT"
   }
 }
+const enemyMoveLeft = (enemy) => {
+  return {
+    type: "ENEMY_MOVE_LEFT",
+    payload: enemy
+  }
+}
+const enemyMoveRight = (enemy) => {
+  return {
+    type: "ENEMY_MOVE_RIGHT",
+    payload: enemy
+  }
+}
+const enemyMoveUp = (enemy) => {
+  return {
+    type: "ENEMY_MOVE_UP",
+    payload: enemy
+  }
+}
+const enemyMoveDown = (enemy) => {
+  return {
+    type: "ENEMY_MOVE_DOWN",
+    payload: enemy
+  }
+}
 
 export {
-  allEnemiesAct
+  allEnemiesAct,
+  enemyMoveLeft,
+  enemyMoveRight,
+  enemyMoveUp,
+  enemyMoveDown
 }
--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -6,11 +6,70 @@ function reducer (state = initialState, action) {
 
   var newState = Object.assign({}, state)
   var { tileGrid, enemies, player } = newState
+  //can we refactor these i and j things? it seems like they
+  //should be taken out of newState and then directly modified
+  // could destructure them?
   var i = state.player.position.y
   var j = state.player.position.x
   var nextTile
 
-  var nextLevelFunc = function() {
+function moveLeft (enemy) {
+  enemy.position.x --
+}
+
+function moveRight (enemy) {
+  enemy.position.x ++
+}
+
+function moveUp (enemy) {
+  enemy.position.y --
+}
+
+function moveDown (enemy) {
+  enemy.position.y ++
+}
+
+function moveTowardsPlayer(enemy) {
+    var {x, y} = enemy.position
+    var presentEnemy
+    //psuedocode:
+    // the case where the player is adjacent to the enemy should already have been handled. So:
+    // first implementation,
+    // if the enemy's x is greater than the players
+    //  check if the square to the left is a room tile
+    //  if  it is, move left
+    nextTile = tileGrid[y][x-1]
+    presentEnemy = enemies.find(function(enemy){return enemy.position.x==x-1 && enemy.position.y==y})
+    if (j < x && nextTile == 1 && !presentEnemy){
+      moveLeft(enemy)
+    }
+    // else
+    // if the enemy's x is less than the players
+    //  check if the square to the right is a room tile
+    //  if it is, move right
+    presentEnemy = enemies.find(function(enemy){return enemy.position.x==x+1 && enemy.position.y==y})
+    if (j > x && tileGrid[y][x+1] == 1 && !presentEnemy){
+      moveRight(enemy)
+    }
+    // else
+    // if the enemy's y is less than the players
+    //  check if the square to the bottom is a room tile
+    //  if it is, move down
+    presentEnemy = enemies.find(function(enemy){return enemy.position.x==x && enemy.position.y==y+1})
+    if (i > y && tileGrid[y+1][x] == 1 && !presentEnemy){
+      moveDown(enemy)
+    }
+    // else
+    // if the enemy's y is greater than the players
+    //  check if the square to the top is a room tile
+    //  if it is, move up
+    presentEnemy = enemies.find(function(enemy){return enemy.position.x==x && enemy.position.y==y-1})
+    if (i < y && tileGrid[y-1][x] == 1 && !presentEnemy){
+      moveUp(enemy)
+    }
+  }
+
+  const nextLevelFunc = () => {
     newState.currentLevel ++
     if (newState.currentLevel == 5){
       newState.display = "win"
@@ -18,55 +77,6 @@ function reducer (state = initialState, action) {
     }
     newState.tileGrid = levelGrids[newState.currentLevel-2]
     return newState
-  }
-
-  const enemyMoveX = (enemy) => {
-
-  }
-
-  const moveTowardsPlayer = (enemy) => {
-    var {x, y} = enemy.position
-    //psuedocode:
-    // the case where the player is adjacent to the enemy should already have been handled. So:
-    // first implementation,
-    // if the enemy's x is greater than the players
-    //  check if the square to the left is a room tile
-    //  if it is, move left
-    // else
-    // if the enemy's x is less than the players
-    //  check if the square to the right is a room tile
-    //  if it is, move right
-    // else
-    // if the enemy's y is greater than the players
-    //  check if the square to the bottom is a room tile
-    //  if it is, move down
-    // else
-    // if the enemy's y is less than the players
-    //  check if the square to the top is a room tile
-    //  if it is, move up
-    console.log(tileGrid[y][x-1] == 1)
-    if (j < x && tileGrid[y][x-1] == 1){
-      x--
-    }
-    //if (j < x){
-    //   nextTile = tileGrid[y][x-1]
-    //   if(nextTile == 1){
-    //     x--
-    //   }
-    // return
-    // }if (i > y){
-    //   nextTile = tileGrid[y-1][x]
-    //   if(nextTile == 1){
-    //     y--
-    //   }
-    //   return
-    // }if (i < y){
-    //   nextTile = tileGrid[y+1][x]
-    //   if(nextTile == 1){
-    //     y++
-    //   }
-    // return
-    // }
   }
 
   switch(action.type){
@@ -123,6 +133,7 @@ function reducer (state = initialState, action) {
         })
         newState.enemies.splice(enemyIndex, 1)
       }
+      console.log('attacking', action.payload)
       return newState
 
     //these are the cases for enemies attacking
@@ -132,15 +143,14 @@ function reducer (state = initialState, action) {
         //this needs to go through the enemies and tell them all to act, and the enemy should handle the logic?
         //because each enemy is a simple object, handling the logic in here for now. COULD GET BULKY
         // thought: could this action just make each enemy dispatch an action of its own?
-
-        if (moveTowardsPlayer(enemy)){
-
-        }if(  enemy.position.x == j+1 && enemy.position.y == i ||
+        if(  enemy.position.x == j+1 && enemy.position.y == i ||
              enemy.position.x == j-1 && enemy.position.y == i ||
              enemy.position.x == j && enemy.position.y == i-1 ||
              enemy.position.x == j && enemy.position.y == i+1    ){
                player.health--
-          }
+        } else {
+          moveTowardsPlayer(enemy)
+        }
       })
       return newState
 
