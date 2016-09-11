59bcbba601ab952edb1f33813371ff1303b59591
Hudson Prestidge <charcon@ihug.co.nz>
2016-09-11 15:24:17 +1200

    connector added

---
+++ client/connectors/ItemConnector.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Item from '../components/item'
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  }
+}
+const itemConnector = connect(mapStateToProps)(Item)
+
+export default itemConnector
