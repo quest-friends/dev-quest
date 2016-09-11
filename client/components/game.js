cbe74848268988d60f8ade7a6d9a29661e7eaa0f
Hudson Prestidge <charcon@ihug.co.nz>
2016-09-11 23:49:20 +1200

    refactor eventlistener so it doesn't repeat itself so much

--- client/components/game.js
+++ client/components/game.js
@@ -10,6 +10,10 @@ class Game extends React.Component {
 
   }
 
+  isArrowKey(key) {
+    return (key == 'ArrowLeft' || key == 'ArrowRight' || key == 'ArrowDown' || key == 'ArrowUp' )
+  }
+
   isPlayerDead() {
     return this.props.player.health <= 0
   }
@@ -19,84 +23,45 @@ class Game extends React.Component {
     var { enemies, player } = this.props
     var presentEnemy
 
-    //This works for firefox
+    //This removes defaults for firefox
     document.addEventListener("keypress", (evt) => {
-      if(evt.key == 'ArrowLeft' || evt.key == 'ArrowRight' || evt.key == 'ArrowDown' || evt.key == 'ArrowUp' ){
+      if(this.isArrowKey(evt.key)){
         evt.preventDefault()
       }
     })
 
-    //This works for Chrome
-    document.addEventListener("keydown", (evt) => {
-      if(evt.key == 'ArrowLeft' || evt.key == 'ArrowRight' || evt.key == 'ArrowDown' || evt.key == 'ArrowUp' ){
-        evt.preventDefault()
-      }
-    })
-
+    //This removes defaults for Chrome
     document.addEventListener("keydown", (evt) => {
         var {y, x} = player.position
-
-      switch(evt.key) {
-
-        case('ArrowLeft'):
+        if (this.isArrowKey(evt.key)) {
+          evt.preventDefault()
+          var newPosition = {x, y}
+          switch(evt.key){
+            case 'ArrowLeft':
+              newPosition = {x:x-1,y:y}
+              break;
+            case 'ArrowRight':
+              newPosition = {x:x+1,y:y}
+              break;
+            case 'ArrowUp':
+              newPosition = {x:x,y:y-1}
+              break;
+            case 'ArrowDown':
+              newPosition = {x:x,y:y+1}
+          }
           presentEnemy = enemies.find(function(enemy) {
-            return (enemy.position.y == y && enemy.position.x == x-1)
+            return (enemy.position.y == newPosition.y && enemy.position.x == newPosition.x)
           })
           if(presentEnemy) {
             this.props.playerAttack(presentEnemy)
           }else{
-            this.props.playerMove( y, x - 1 )
+            this.props.playerMove( newPosition.y, newPosition.x )
           }
           this.props.allEnemiesAct()
           if(this.isPlayerDead()){
             this.props.loseGame()
           }
-          break;
-
-        case('ArrowRight'):
-          presentEnemy = enemies.find(function(enemy) {
-            return (enemy.position.y == player.position.y && enemy.position.x == player.position.x+1)
-          })
-          if(presentEnemy) {
-            this.props.playerAttack(presentEnemy)
-          }else{
-            this.props.playerMove( y, x + 1 )
-          }
-          this.props.allEnemiesAct()
-          if(this.isPlayerDead()){
-            this.props.loseGame()
-          }
-          break;
-
-        case('ArrowUp'):
-          presentEnemy = enemies.find(function(enemy) {
-            return (enemy.position.y == player.position.y-1 && enemy.position.x == player.position.x)
-          })
-          if(presentEnemy) {
-            this.props.playerAttack(presentEnemy)
-          }else{
-            this.props.playerMove( y - 1, x )
-          }
-          this.props.allEnemiesAct()
-          if(this.isPlayerDead()){
-            this.props.loseGame()
-          }
-          break;
-
-        case('ArrowDown'):
-          presentEnemy = enemies.find(function(enemy) {
-            return (enemy.position.y == player.position.y+1 && enemy.position.x == player.position.x)
-          })
-          if(presentEnemy) {
-            this.props.playerAttack(presentEnemy)
-          }else{
-            this.props.playerMove( y + 1, x )
-          }
-          this.props.allEnemiesAct()
-          if(this.isPlayerDead()){
-            this.props.loseGame()
-          }
-      }
+        }
     })
   }
 
