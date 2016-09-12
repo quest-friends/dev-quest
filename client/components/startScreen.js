eab75dcf2e6a72975573b435f8574e8a4a0306ca
cam-shotter <cameronshotter@gmail.com>
2016-09-13 11:49:18 +1200

    removed inline breaks

--- client/components/startScreen.js
+++ client/components/startScreen.js
@@ -6,13 +6,10 @@ class StartScreen extends React.Component {
     return (
       <div className="displayScreen">
         <h1>Dev Quest 2.0</h1>
-        <br /><br /><br />
         <h4>The path to coding mastery is fraught with trials and danger.
           It will take all of your determination, skill, and coffee to achieve greatness.</h4>
-        <br />
         <h2>Are you ready to begin your journey?</h2>
         <button className="startGameButton" onClick={this.props.startGame}>Let's Code!</button>
-        <br />
       </div>
     )
   }
--- client/components/winScreen.js
+++ client/components/winScreen.js
@@ -6,9 +6,7 @@ class WinScreen extends React.Component {
     return (
       <div className="displayScreen">
         <h1>Well done, you have reached coding enlightenment!</h1>
-        <br /><br /><br />
         <h4>It seems your journey as a Web Developer has come to an end</h4>
-        <br />
         <h2>Give it another go?</h2>
         <button className="startGameButton" onclick={this.props.restartGame}>Refactor Your Code</button>
       </div>
