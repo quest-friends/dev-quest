c16a996c7c1bded97c670bf377b2a153e8c98ccd
cam-shotter <cameronshotter@gmail.com>
2016-09-13 19:09:48 +1200

    fixed player.js syntax error

--- client/players/players.js
+++ client/players/players.js
@@ -1,28 +1,28 @@
 module.exports = {
 
-JavaScript: {
-  type: "JavaScript",
-  health: 10,
-  charge: 50,
-  xp: 0,
-  attack: 2,
-  defence: 1,
-  messages: {
-    messageOnPlayerChoice: "You've chosen JavaScript, good choice!",
-    messageOnXpIncrease: "You can now write in ES6 - now you can slay const and let"
-  }
-},
-
-Ruby: {
-  type: "Ruby",
-  health: 20,
-  charge: 20,
-  xp: 0,
-  attack: 1,
-  defence: 2,
-  messages: {
-    messageOnPlayerChoice: "Hmm Ruby - hope you like playing by the rules...",
-    messageOnXpIncrease: "Your jewel box is now full of Gems!"
+  JavaScript: {
+    type: "JavaScript",
+    health: 10,
+    charge: 50,
+    xp: 0,
+    attack: 2,
+    defence: 1,
+    messages: {
+      messageOnPlayerChoice: "You've chosen JavaScript, good choice!",
+      messageOnXpIncrease: "You can now write in ES6 - now you can slay const and let"
+    }
+  },
+  Ruby: {
+    type: "Ruby",
+    health: 20,
+    charge: 20,
+    xp: 0,
+    attack: 1,
+    defence: 2,
+    messages: {
+      messageOnPlayerChoice: "Hmm Ruby - hope you like playing by the rules...",
+      messageOnXpIncrease: "Your jewel box is now full of Gems!"
+    }
   }
 
 }
