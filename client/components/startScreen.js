485b3f78fa7d78b6c058ba5d63c6b3d61feaa57e
cam-shotter <cameronshotter@gmail.com>
2016-09-13 19:05:46 +1200

    starting class selectors have beginning stats and a space for images

--- client/components/startScreen.js
+++ client/components/startScreen.js
@@ -12,16 +12,37 @@ class StartScreen extends React.Component {
           <h1>Dev Quest 2.0</h1>
           <p>The path to coding mastery is fraught with trials and danger.
             It will take all of your determination, skill, and coffee to achieve greatness.</p>
-          <h2>Are you ready to begin your journey?</h2>
+          <h2>Are you ready to begin your journey as a junior dev?</h2>
         </div>
 
         <div className="new-player-class">
           <div className="class-info">
+            <div className="class-picture">
+              <img alt="JavaScript"></img>
+            </div>
+            <div className="class-starting-stats">
+              <h3>Beginner Stats</h3>
+              <h5>Wellbeing: 10</h5>
+              <h5>Attack: 2</h5>
+              <h5>Defence: 1</h5>
+              <h5>Laptop Charge: 50%</h5>
+            </div>
             <button className="start-game-button" onClick={ () => this.props.startGame(players.JavaScript) } >Code in JavaScript</button>
           </div>
         </div>
+
         <div className="new-player-class">
           <div className="class-info">
+            <div className="class-picture">
+              <img alt="Ruby"></img>
+            </div>
+            <div className="class-starting-stats">
+              <h3>Beginner Stats</h3>
+              <h5>Wellbeing: 20</h5>
+              <h5>Attack: 1</h5>
+              <h5>Defence: 2</h5>
+              <h5>Laptop Charge: 20%</h5>
+            </div>
             <button className="start-game-button" onClick={ () => this.props.startGame(players.Ruby) } >Code in Ruby</button>
           </div>
         </div>
