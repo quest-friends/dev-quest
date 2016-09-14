c87a5eba47b6633a980b8dfd4659b51ba119359f
madeleine-rashbrooke <madeleine.rashbrooke@gmail.com>
2016-09-14 20:07:29 +1200

    Added lossScreenConnector and started making loss screen provide info on why player lost

--- client/components/lossScreen.js
+++ client/components/lossScreen.js
@@ -6,15 +6,20 @@ class LossScreen extends React.Component {
     location.reload()
   }
 
+  textToRender() {
+    //if health <= 0, render "You neglected your health & well-being - developers need to take care of themselves as well as writing sweet code"
+    //if charge <= 0, text is "You ran out of charge - developers need to keep their (literal AND figurative) batteries topped up"
+  }
+
   render(){
     return (
       <div className="display-screen">
         <div className="display-screen-info">
           <h1>Oh no, you lost!</h1>
           <p>It seems your journey as a Web Developer has come to an end</p>
-          <h2>Give it another go?</h2>
+          <h2>Maybe it's time to refactor your code?</h2>
         </div>
-        <button className="start-game-button" onClick={this.reloadGame}>Refactor Your Code</button>
+        <button className="start-game-button" onClick={this.reloadGame}>`Start new game`</button>
       </div>
     )
   }
