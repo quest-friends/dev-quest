3b51728696f5f637dc2cbc1c7f063b063e144da7
cam-shotter <cameronshotter@gmail.com>
2016-09-14 14:45:50 +1200

    general style changes

--- client/components/playerStats.js
+++ client/components/playerStats.js
@@ -5,12 +5,16 @@ class PlayerStats extends React.Component {
   render(){
     return (
       <div className='ui-playerStats'>
-        <h3>Coder Stats:</h3>
-        <p>- Main Language: {this.props.type}</p>
-        <p>- Wellbeing: {this.props.health}</p>
-        <p>- Laptop Charge: {this.props.charge}%</p>
-        <p>- Attack: {this.props.attack}</p>
-        <p>- Exp: {this.props.xp}</p>
+        <div className="ui-heading">
+          <h3>Coder Stats:</h3>
+        </div>
+        <div className="ui-statsLog">
+          <p>- Main Language: {this.props.type}</p>
+          <p>- Wellbeing: {this.props.health}</p>
+          <p>- Laptop Charge: {this.props.charge}%</p>
+          <p>- Attack: {this.props.attack}</p>
+          <p>- Exp: {this.props.xp}</p>
+        </div>
       </div>
     )
   }
